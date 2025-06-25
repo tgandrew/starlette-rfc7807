673931edcfa8e0b3af38c08b6c4f5fbe6ded91dc
Tom <624664+tgandrew@users.noreply.github.com>
2025-06-24 21:38:36 -0700

    Try and fix publishing

--- starlette_rfc7807/__init__.py
+++ starlette_rfc7807/__init__.py
@@ -8,11 +8,4 @@ to generate RFC 7807 compliant problem detail responses.
 from .exception import Problem
 from .middleware import ProblemMiddleware, http_exception_handler
 
-try:
-    from importlib.metadata import version
-
-    __version__ = version("starlette-rfc7807")
-except ImportError:
-    __version__ = "unknown"
-
 __all__ = ["Problem", "http_exception_handler", "ProblemMiddleware"]
